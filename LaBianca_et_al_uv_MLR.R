c93e62f113a40b2f9876a31d182b0455d936a49f
Andrew Schork <36660856+AndrewSchork@users.noreply.github.com>
2021-10-27 09:37:49 +0200

    Create LaBianca_et_al_uv_MLR.R

---
+++ LaBianca_et_al_uv_MLR.R
@@ -0,0 +1,2 @@
+## Univariate multinomial logistic regression as applied in LaBianca et al for the GWAS applications
+## univariate used to refer to a single variable of interest (SNP) despite the presence of jointly fit covariates
